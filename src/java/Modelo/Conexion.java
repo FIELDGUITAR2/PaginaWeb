f395cec8a2591eed45d21de77af524800a7f4ac6
FIELDGUITAR2 <josagoor@gmail.com>
2025-02-06 17:00:40 -0500

    Añadir Conexion BD

---
+++ src/java/Modelo/Conexion.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author fieldguitar
+ */
+public class Conexion {
+    
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String bd = "bd_biblioteca"; 
+    private static String passw = "#Kaliman2"; 
+    private static String usuario = "root";
+    private static String url = "jdbc:mysql://localhost:3306/"+bd;
+    
+    public Connection conex;
+    public PreparedStatement stm;
+    
+    public Conexion(){
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se pudo crear el puente");
+        }
+        try {
+            conex = DriverManager.getConnection(url,usuario,passw);
+        } catch (SQLException e) {
+            System.out.println("Error: "+e);
+        }
+    }
+    
+    public Connection getConex() {
+        return conex;
+    }
+
+    public void setConex(Connection conex) {
+        this.conex = conex;
+    }
+
+    public PreparedStatement getStm() {
+        return stm;
+    }
+
+    public void setStm(PreparedStatement stm) {
+        this.stm = stm;
+    }
+    
+}
